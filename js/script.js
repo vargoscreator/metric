276e90ac58e926b8c922e1e6d093758055ee8169
vargoscreator <bacahoriyk@gmail.com>
2023-03-07 09:22:59 +0200

    commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+﻿let bodyclick = document.querySelector('.body');
+let header__burger = document.querySelector('.header__burger');
+let header_menu = document.querySelector('.header__menu');
+let header__overlay = document.querySelector('.header__overlay');
+header__burger.onclick = function(){
+  changeClass();
+}
+header__overlay.onclick = function(){
+  changeClass();
+}
+function changeClass(){
+  bodyclick.classList.toggle('lock');
+    header__burger.classList.toggle('active');
+    header_menu.classList.toggle('active');
+    header__overlay.classList.toggle('lock');
+    header__list.classList.toggle('active');
+}
